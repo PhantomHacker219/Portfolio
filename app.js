d42c96c8c7ad2a0af20fdf96b7cc903979a2ebed
PhantomHacker219 <jbrodi766@gmail.com>
2024-09-28 17:35:05 +0500

    add media screen

--- app.js
+++ app.js
@@ -1,49 +1,40 @@
 let text = "Портфолио";
-let Name = "Я - Frontend разработчик"
-let LongText = "Мотивированный Frontend-разработчик с опытом создания веб-сайтов на заказ. Обладаю глубокими знаниями JavaScript и React.js. Стремлюсь непрерывно совершенствоваться и расти в профессии, чтобы достичь уровня Senior-разработчика."
-let h = 0;
-let v = 0;
-let i = 0;
-let j = 0;
+let Name = "Я - Frontend разработчик";
+let LongText = "Мотивированный Frontend-разработчик с опытом создания веб-сайтов на заказ. Обладаю глубокими знаниями JavaScript и React.js. Стремлюсь непрерывно совершенствоваться и расти в профессии, чтобы достичь уровня Senior-разработчика.";
+let h = 0; 
+let i = 0; 
+let v = 0; 
+let l = 0; 
 let speed = 150; 
-let speed2 = 175;
 let speed3 = 190;
 let speed4 = 45;
-
 
 function typeWriter() {
   if (i < text.length) {
     document.getElementById("typing").innerHTML += text.charAt(i);
     i++;
     setTimeout(typeWriter, speed);   
-
   }
 }
 
 typeWriter();
-  
-
-
 
 function TypeName() {
     if(v < Name.length){
-        document.getElementById("Name").innerHTML +=    Name.charAt(v);
+        document.getElementById("Name").innerHTML += Name.charAt(v);
         v++;
-        setTimeout(TypeName, speed3)
+        setTimeout(TypeName, speed3);
     }
 }
 
 TypeName();
 
-
-
 function TypeLongText() {
-    if(h < LongText.length){
-        document.getElementById("LongText").innerHTML += LongText.charAt(h);
-        h++;
-        
-    }
-    setTimeout(TypeLongText, speed4)
+  if (l < LongText.length) {
+    document.getElementById("LongText").innerHTML += LongText.charAt(l); // здесь добавьте уникальный id
+    l++;
+    setTimeout(TypeLongText, speed4);
+  }
 }
 
 TypeLongText();
